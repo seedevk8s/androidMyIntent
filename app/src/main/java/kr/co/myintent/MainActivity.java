ec8732a13a63d8493fb72c2e11f990f0940b29ce
seedevk8s <seedevk8s@gmail.com>
2022-02-02 12:57:23 +0900

    Project 10-2 명시적 인텐트(명화선호도 투표 앱)

---
+++ app/src/main/java/kr/co/myintent/MainActivity.java
@@ -0,0 +1,87 @@
+package kr.co.myintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.ic_launcher);
+        setTitle("명화 선호도 투표");
+
+        final int[] voteCount = new int[9];
+        for (int i=0; i<9; i++) {
+            voteCount[i] = 0;
+        }
+
+
+        // 9개의 이미지 버튼 객체배열
+        ImageView[] imageView = new ImageView[9];
+        // 9개의 이미지버튼 ID 배열
+        Integer[] imageId = {R.id.iv1, R.id.iv2, R.id.iv3, R.id.iv4, R.id.iv5,
+                            R.id.iv6, R.id.iv7, R.id.iv8, R.id.iv9};
+        final String[] imagName = {"독서하는 소녀", "꽂장식 소녀", "부채를 든 소녀", "이레느깡 단 베르양", "잠자는 소녀",
+                                    "테라스의 두 자매", "피아노 레슨", "피아노 앞의 소녀들", "해변에서"};
+
+        for (int i = 0; i < imageId.length; i++) {
+            final int index;            // 꼭 필요함
+            index = i;
+
+            imageView[index] = findViewById(imageId[index]);
+            imageView[index].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    //투표수 증가
+                    voteCount[index]++;
+                    Toast.makeText(getApplicationContext(), imagName[index] +": 총 "+ voteCount[index]+ " 표", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+
+        Button btnFinish = findViewById(R.id.btnResult);
+        btnFinish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+                intent.putExtra("VoteCount", voteCount);
+                intent.putExtra("ImageName", imagName);
+                startActivity(intent);
+            }
+        });
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/kr/co/myintent/ResultActivity.java
@@ -0,0 +1,72 @@
+package kr.co.myintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+        getSupportActionBar().setIcon(R.drawable.ic_launcher);
+        setTitle("명화 선호도 투표 결과");
+
+        // 앞 화면에서 보낸 투표 결과 값을 받음
+        Intent intent = getIntent();
+        int[] voteResult = intent.getIntArrayExtra("VoteCount");
+        String[] imageName = intent.getStringArrayExtra("ImageName");
+
+        // 9개의 TextView, RationBar 객체배열 생성
+        TextView[] textViews = new TextView[imageName.length];
+        RatingBar[] ratingBars = new RatingBar[imageName.length];
+
+        // 9개의 TextView, RationBar ID 배열 생성
+        Integer[] tvID = {R.id.tv1, R.id.tv2, R.id.tv3, R.id.tv4, R.id.tv5,
+                        R.id.tv6, R.id.tv7, R.id.tv8, R.id.tv9};
+        Integer[] rbarID = {R.id.rbar1, R.id.rbar2, R.id.rbar3, R.id.rbar4, R.id.rbar5,
+                            R.id.rbar6, R.id.rbar7, R.id.rbar8, R.id.rbar9};
+
+        // 9개의 TextView, RationBar 개체 연결하기
+        for (int i = 0; i < voteResult.length; i++) {
+            textViews[i] = findViewById(tvID[i]);
+            ratingBars[i] = findViewById(rbarID[i]);
+        }
+
+        // 각 TextView 및 RationBar에 넘겨 받은 값을 반영하기
+        for (int i = 0; i < voteResult.length; i++) {
+            textViews[i].setText(imageName[i]);
+            ratingBars[i].setRating((float) voteResult[i]);
+        }
+
+        Button btnReturn = findViewById(R.id.btnReturn);
+        btnReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
